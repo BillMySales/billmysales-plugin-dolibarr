68f3f9a6cac9259f20ba7493d81e0a1ec3065915
nbcontreras <nico.contrerasb@gmail.com>
2022-01-07 15:42:44 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,25 @@
+Plugin BillMySales para Dolibarr
+==================================
+
+Este módulo realiza el envío de los datos de una factura a [BillMySales](https://billmysales.com)
+para realizar el proceso de facturación del pedido.
+
+El envío de los datos se realiza cada vez que el pedido cambia a estado pagado y se
+procesará según las reglas definidias en la pasarela de facturación en BillMySales.
+
+
+![Configuración del módulo en Dolibarr](https://i.imgur.com/nIK435t.png "Configuración del módulo en Dolibarr")
+
+Instalar a partir de este código fuente
+---------------------------------------
+
+1. Descargar el [código de este repositorio](https://github.com/BillMySales/billmysales-plugin-prestashop/archive/refs/heads/main.zip).
+2. Descomprimir el archivo y renombrar la carpeta a `module_billmysales-1.0.0`.
+3. Comprimir la carpeta renombrada.
+4. Subir el módulo a Dolibarr.
+
+Licencia
+--------
+
+Este código está liberado bajo la licencia de software libre [AGPL](http://www.gnu.org/licenses/agpl-3.0.en.html).
+Para detalles sobre cómo se puede utilizar, modificar y/o distribuir este plugin revisar los términos de la licencia.
---
+++ core/triggers/interface_99_modBillmysales_BillmysalesTriggers.class.php
@@ -0,0 +1,143 @@
+<?php
+/* Copyright (C) 2022 SuperAdmin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * \file    core/triggers/interface_99_modBillmysales_BillmysalesTriggers.class.php
+ * \ingroup billmysales
+ * \brief   Example trigger.
+ *
+ * Put detailed description here.
+ *
+ * \remarks You can create other triggers by copying this one.
+ * - File name should be either:
+ *      - interface_99_modBillmysales_MyTrigger.class.php
+ *      - interface_99_all_MyTrigger.class.php
+ * - The file must stay in core/triggers
+ * - The class name must be InterfaceMytrigger
+ * - The constructor method must be named InterfaceMytrigger
+ * - The name property name must be MyTrigger
+ */
+
+require_once DOL_DOCUMENT_ROOT.'/core/triggers/dolibarrtriggers.class.php';
+
+
+/**
+ *  Class of triggers for Billmysales module
+ */
+class InterfaceBillmysalesTriggers extends DolibarrTriggers
+{
+	/**
+	 * Constructor
+	 *
+	 * @param DoliDB $db Database handler
+	 */
+	public function __construct($db)
+	{
+		$this->db = $db;
+
+		$this->name = preg_replace('/^Interface/i', '', get_class($this));
+		$this->family = "demo";
+		$this->description = "Billmysales triggers.";
+		// 'development', 'experimental', 'dolibarr' or version
+		$this->version = 'development';
+		$this->picto = 'billmysales@billmysales';
+	}
+
+	/**
+	 * Trigger name
+	 *
+	 * @return string Name of trigger file
+	 */
+	public function getName()
+	{
+		return $this->name;
+	}
+
+	/**
+	 * Trigger description
+	 *
+	 * @return string Description of trigger file
+	 */
+	public function getDesc()
+	{
+		return $this->description;
+	}
+
+
+	/**
+	 * Function called when a Dolibarrr business event is done.
+	 * All functions "runTrigger" are triggered if file
+	 * is inside directory core/triggers
+	 *
+	 * @param string 		$action 	Event action code
+	 * @param CommonObject 	$object 	Object
+	 * @param User 			$user 		Object user
+	 * @param Translate 	$langs 		Object langs
+	 * @param Conf 			$conf 		Object conf
+	 * @return int              		<0 if KO, 0 if no triggered ran, >0 if OK
+	 */
+	public function runTrigger($action, $object, User $user, Translate $langs, Conf $conf)
+	{
+	    $methodName = 'trigger'.ucfirst(str_replace(' ', '', ucwords(str_replace('_', ' ', strtolower($action)))));
+		$callback = array($this, $methodName);
+		// si el método existe se llama
+		if (is_callable($callback)) {
+			return call_user_func($callback, $object, $user, $langs, $conf);
+		};
+        // no se encontró método para procesar el webhook
+		return 0;
+	}
+
+	private function triggerBillPayed($facture, $user, $langs, $conf)
+	{
+	    $societe = new Societe($this->db);
+	    $societe->fetch($facture->socid);
+	    $data = json_encode([
+            'facture' => $facture,
+	        'societe' => $societe,
+	        //'billing_contact_id' => $facture->getIdBillingContact(),
+	        //'shipping_contact_id' => $facture->getIdShippingContact(),
+	    ]);
+		if ($conf->global->BILLMYSALES_WEBHOOK_LOG == "1") {
+    	    dol_syslog(
+    				"Trigger '".$this->name."' for action '$action' launched by ".__FILE__.". id=".$facture->id." json_invoice:".$data
+    		);
+	    }
+	    $signature = $this->sign_data($data, $conf->global->BILLMYSALES_WEBHOOK_TOKEN);
+	    $opciones = array(
+            "http" => array(
+                "header" => array(
+                        "Content-type: plain/text",
+                        "X-DolibarrBMS-Hmac-Sha256:".$signature,
+                    ),
+                "method" => "POST",
+                "content" => $data,
+            ),
+        );
+        $contexto = stream_context_create($opciones);
+        $resultado = file_get_contents($conf->global->BILLMYSALES_WEBHOOK_URL, false, $contexto);
+        if ($resultado === false) {
+            return 1;
+        }
+        return 0;
+	}
+
+	private function sign_data($data, $token)
+	{
+        return base64_encode(hash_hmac('sha256', trim($data), $token, true));
+	}
+}
